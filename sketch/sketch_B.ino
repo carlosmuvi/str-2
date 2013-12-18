fe8535b914b1bff48d41c3bb46af30da036405df
Cesar de la Vega <cesarvegaro@gmail.com>
2013-12-18 11:52:50 +0100

    Modifica el isr del 2

--- sketch/sketch_B.ino
+++ sketch/sketch_B.ino
@@ -10,21 +10,15 @@ unsigned long timeOrig;
 
 ISR(TIMER1_COMPA_vect)
 {
-  static int bitwise = 1;
   static unsigned char data = 0;
-    
-    bitwise = (bitwise * 2);
-    if (bitwise > 128) {
-        bitwise = 1;
-        if (Serial.available()>1) {
-           data = Serial.read();
-        }
-    }
+  if (Serial.available()>1) {
+    data = Serial.read();
     if (is_muted) {
-        digitalWrite(soundPin, (data & bitwise) );
+      OCR2A = (byte) data;
     } else {
-        digitalWrite(soundPin, 0 );
+      OCR2A = (byte) 0;
     }
+  }
 }
 
 void mute(){
